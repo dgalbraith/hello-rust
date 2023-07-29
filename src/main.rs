05bf7d93cfe8b141d60ebcdc9452aaff64248dab
David Galbraith <dg@lbraith.io>
2023-07-30 00:09:29 +0100

    Add unit tests
    The initial implementation did not contain any unit tests.
    
    Re-factored the code to extract the domain code and
    implemented a unit test to validate the expected greeting.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+fn greet() -> String {
+    String::from("Hello, world!")
+}
+
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hello, world!");
+    let message = greet();
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::greet;
+
+    #[test]
+    fn test_greet() {
+        assert_eq!(greet(), "Hello, world!");
+    }
 }
